ff24814c0b80a34383e8208062c012684ca541df
gapaza <gapaza@tamu.edu>
2019-10-01 19:31:00 -0500

    files

---
+++ src/main/java/seakers/explorerserver/ga/GASearch.java
@@ -0,0 +1,45 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// This class will have two functions
+// 1. Evaluate a Binary Input GA
+// 2. Evaluate a Discrete Input GA
+public class GASearch 
+{
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
---
+++ src/main/java/seakers/explorerserver/server/JavaServer.java
@@ -0,0 +1,62 @@
+
+
+
+
+
+
+
+
+
+
+
+
+public class JavaServer 
+{
+
+
+
+	// This main function will be continuously checking to see if a GA request was sent to the AWS Queue
+	// When a message is recieved, is will call a function that will start an instance of the GA
+	public static void main(String [] args) 
+	{
+
+
+
+
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	// This function will start the instabce of the GA class
+	public static void start_ga(String problem, String username)
+	{
+
+
+
+
+
+
+
+	}
+
+
+
+
+
+
+
+
+
+}
